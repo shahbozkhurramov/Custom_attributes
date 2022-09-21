6b34565b37430c003b7b73e35d4329ad3ff54f6d
shahbozkhurramov <shahbozkhurramov@gmail.com>
2022-09-21 15:30:49 +0500

    attributes in c#

---
+++ Attributes/RequiredNotEmptyOrWhiteSpaceAttribute.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AttributesWebApi.Attributes;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+public sealed class RequiredNotEmptyOrWhiteSpaceAttribute : RequiredAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is string stringValue)
+        {
+            return !string.IsNullOrWhiteSpace(stringValue);
+        }
+
+        return base.IsValid(value);
+    }
+}
---
+++ Attributes/ValidateModelAttribute.cs
@@ -0,0 +1,18 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SMG.Web.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class ValidateModelAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            context.Result = new BadRequestObjectResult(context.ModelState);
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        }
+    }
+}
